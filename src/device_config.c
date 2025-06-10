39e5ead873b9cdf6c1422116331faf8634507b30
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-06-10 16:32:12 -0400

    Update device_config.c

--- src/device_config.c
+++ src/device_config.c
@@ -1,27 +1,14 @@
 #include "device_config.h"
 
 void device_config(void){
-    // Turn off analog on all ports
-    ANSELA = OFF;
-    ANSELB = OFF;
-    ANSELC = OFF;
-
-
-    // Clear all inputs
-    INPUT_LAT = CLR;
-    LED_LAT =  CLR;
-    LASER_LAT = CLR;
-    
-    // Set I/O directions
-    LASER_TRIS = OUTPUT;  // All PORTA as outputs (e.g., LASER)
-    LED_TRIS = OUTPUT;  // All PORTB as outputs (e.g., LEDs)
-    INPUT_TRIS = INPUT;  // All PORTC as inputs (e.g., phototransistor)
-
-    // Define inputs and outputs
+// === Pin Setup ===
     PHOTOTRANSISTOR = INPUT;
-    LED_1 = OUTPUT;
+    ANSELCbits.ANSELC7 = 0;
+
+    LED_OUT1 = OUTPUT;
+    ANSELBbits.ANSELB0 = 0;
+
     ERROR_LED = OUTPUT;
+    ANSELBbits.ANSELB7 = 0;
 
-
-    
 }
